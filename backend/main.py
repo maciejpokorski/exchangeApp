db08d1001afe2a35dd5f4dd012fa244d955f5ff2
Maciej Pokorski <maciekpokorski@gmail.com>
2023-09-07 18:16:04 +0200

    Pass env into app

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
+import os
 from typing import Union
+from functools import lru_cache
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from typing_extensions import Annotated
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": os.getenv("API_KEY")}
