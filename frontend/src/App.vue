1bb79b8af6ff3d81ae32d1a4cbf631838ca67846
Maciej Pokorski <maciekpokorski@gmail.com>
2023-09-08 13:56:20 +0200

    fetch data from local api and display it

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+      <div class="row pb-5">
+        <div class="col"><button type="button" class="btn btn-primary" @click="fetchData">Reload data</button></div>
+      </div>
+      <div class="row">
+        <div class="col d-flex align-items-center">
+          <div>
+            <Datepicker v-model="date" :enable-time-picker="false" :max-date="new Date()"/>
+          </div>
+          <div>
+            <ExchangeRatesInline :exchangeRates="exchangeRates" :isLoading="isLoading"></ExchangeRatesInline>
+          </div>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { toValue } from 'vue';
+import ExchangeRatesInline from './components/ExchangeRatesInline.vue'
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ExchangeRatesInline,
+    Datepicker
+  },
+  data() {
+    return {
+        date: null,
+        exchangeRates: null,
+        isLoading: true,
+    };
+  },
+  mounted() {
+    this.date = new Date()
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const url = toValue("http://localhost:8000/fetch_data/" + this.date.toISOString().slice(0, 10))
+
+      fetch(toValue(url))
+        .then((res) => res.json())
+        .then((json) => (this.exchangeRates = json))
+        .catch((err) => (console.error("Error fetching data: " + err)))
+        .finally(() => (this.isLoading = false));
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ frontend/src/components/ExchangeRatesInline.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div class="container">
+          <div v-if="isLoading" class="d-inline-flex">
+              <span class="list-item pe-3">Loading...</span>
+          </div>
+          <div v-else class="d-inline-flex">
+              <template v-for="(rate, currency) in exchangeRates" :key="currency">
+                  <span class="list-item pe-3">{{ currency }}: {{ rate }}</span>
+              </template>
+          </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      exchangeRates: Object,
+      isLoading: Boolean,
+    },
+  };
+  </script>
