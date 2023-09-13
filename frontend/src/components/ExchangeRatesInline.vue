22f779b7c66dc1f09920447d916fd1de037d810c
Maciej Pokorski <maciekpokorski@gmail.com>
2023-09-13 23:56:56 +0200

    responsive css

--- frontend/src/components/ExchangeRatesInline.vue
+++ frontend/src/components/ExchangeRatesInline.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-      <div class="container">
+      <div class="px-md-3 ps-1 mt-md-0 mt-3">
           <div v-if="isLoading" class="d-inline-flex">
               <span class="list-item pe-3">Loading...</span>
           </div>
-          <div v-else class="d-inline-flex">
+          <div v-else class="d-md-inline-flex d-flex">
               <template v-for="(rate, currency) in exchangeRate" :key="currency">
                   <span class="list-item pe-3">{{ currency }}: {{ rate }}</span>
               </template>
