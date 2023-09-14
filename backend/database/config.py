4dc4738f5aa54cababb78d2612f18506c70ddba0
Maciej Pokorski <maciekpokorski@gmail.com>
2023-09-14 17:23:08 +0200

    add fallback if mysql connection doesn't work

--- backend/database/config.py
+++ backend/database/config.py
@@ -2,12 +2,19 @@ from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
 from constants import USERNAME, PASSWORD, DATABASE
 
-MYSQL_URL = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@db:3306/{DATABASE}"
-engine = create_engine(MYSQL_URL, echo=True)
+MYSQL_URL = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@localhost:3306/{DATABASE}"
+try:
+    engine = create_engine(MYSQL_URL, echo=True)
+except Exception as e:
+    print(f"Error connecting to MYSQL database: {e}")
+finally:
+    engine = create_engine(
+        f"sqlite:///{DATABASE}.db", connect_args={"check_same_thread": False}
+    )
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-def get_session(database_url: str = MYSQL_URL):
+def get_session():
     with Session(engine) as session:
         yield session
