22ff41f641fd2446e83fddd774f7523e22990ccf
Maciej Pokorski <maciekpokorski@gmail.com>
2023-09-14 18:33:50 +0200

    improve task execution

--- backend/database/config.py
+++ backend/database/config.py
@@ -2,15 +2,15 @@ from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
 from constants import USERNAME, PASSWORD, DATABASE
 
-MYSQL_URL = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@localhost:3306/{DATABASE}"
+MYSQL_URL = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@db:3306/{DATABASE}"
 try:
     engine = create_engine(MYSQL_URL, echo=True)
+    print("Connected to MYSQL database!")
 except Exception as e:
-    print(f"Error connecting to MYSQL database: {e}")
-finally:
     engine = create_engine(
         f"sqlite:///{DATABASE}.db", connect_args={"check_same_thread": False}
     )
+    print(f"Error connecting to MYSQL database, use sqlite instead. Error: {e}")
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
--- backend/task.py
+++ backend/task.py
@@ -2,12 +2,12 @@ import datetime
 from fastapi_utils.tasks import repeat_every
 from fastapi import FastAPI, Depends
 from sqlmodel import Session
-from database.config import get_session
+from database.config import engine
 from utils import fetch_remote_data
 from database.models.exchange_rate import ExchangeRate
 from database.service import save_exchange_rate
 
-async def daily_task(session: Session = Depends(get_session)):
+async def daily_task():
     """
     Perform a daily task to fetch and save exchange rate data.
 
@@ -20,7 +20,8 @@ async def daily_task(session: Session = Depends(get_session)):
         today = datetime.date.today().strftime("%Y-%m-%d")
         data = await fetch_remote_data(today)
         exchange_rate = ExchangeRate(date=today, rates=data)
-        save_exchange_rate(session, exchange_rate)
+        with Session(engine) as session:
+            save_exchange_rate(session, exchange_rate)
         print(f"Daily task completed at {datetime.datetime.now()}")
     except Exception as e:
         print(e)
@@ -32,6 +33,7 @@ def setup_daily_task(app: FastAPI):
     Args:
         app (FastAPI): The FastAPI application to add the task to.
     """
+    @app.on_event("startup")
     @repeat_every(seconds=60 * 60 * 24)  # Repeat every 24 hours (1 day)
     async def start_daily_task():
         await daily_task()
